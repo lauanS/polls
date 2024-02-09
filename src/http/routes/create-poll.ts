68d6ec2ffff992c3ea8abc91c7a6671e35704450
lauanS <lauan.s.souza@gmail.com>
2024-02-09 14:21:47 -0300

    :recycle: Moves routes from Server.ts to http/routes

---
+++ src/http/routes/create-poll.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+import { prisma } from '../../lib/prisma';
+import { FastifyInstance } from 'fastify';
+
+export async function createPoll(app: FastifyInstance) {
+  app.post('/poll', async (request, reply) => {
+    console.log('[POST] /poll');
+
+    const createPollBody = z.object({
+      title: z.string()
+    });
+
+    const { title } = createPollBody.parse(request.body);
+
+    const poll = await prisma.poll.create({
+      data: {
+        title
+      }
+    });
+
+    return reply.status(201).send({ pollId: poll.id });
+  });
+}
