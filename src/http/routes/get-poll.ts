48f2bd58121fa215901d6449b1af3232cff26b1e
lauanS <lauan.s.souza@gmail.com>
2024-02-10 16:19:02 -0300

    :sparkles: GetPoll returns the votes for each of its options

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
-import { prisma } from '@/lib/prisma';
 import { FastifyInstance } from 'fastify';
+import { prisma } from '@/lib/prisma';
+import { redis } from '@/lib/redis';
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/poll/:pollId', async (request, reply) => {
@@ -26,6 +27,28 @@ export async function getPoll(app: FastifyInstance) {
       }
     });
 
-    return reply.status(200).send({ poll });
+    if (!poll) {
+      return reply.status(400).send({ message: 'Poll not found' });
+    }
+
+    const rawVotes = await redis.zrange(pollId, 0, -1, 'WITHSCORES');
+    const votes = rawVotes.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = Number(rawVotes[index + 1]);
+        
+        obj[line] = score;
+      }
+
+      return obj;
+    }, {} as Record<string, number>);
+
+    poll.options = poll.options.map((option) => {
+      return {
+        ...option,
+        score: votes[option.id] || 0
+      };
+    });
+    
+    return reply.status(200).send({ poll, votes });
   });
 }
