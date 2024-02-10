78d179827351f96076f0b5e926c0cd7b6a12bcef
lauanS <lauan.s.souza@gmail.com>
2024-02-10 13:44:55 -0300

    :sparkles: Implements a sessionId with cookies to vote in a Poll

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -18,7 +18,19 @@ export async function voteOnPoll(app: FastifyInstance) {
     const { pollId } = voteOnPollParams.parse(request.params);
     const { pollOptionId } = voteOnPollBody.parse(request.body);
 
-    const sessionId = randomUUID();
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true
+      });
+    }
+
 
     await prisma.vote.create({
       data: {
